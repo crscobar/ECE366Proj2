a0011f7f2e391a87822722dbea0eacb361e59f82
dserra8 <47340024+dserra8@users.noreply.github.com>
2019-10-09 13:24:18 -0500

    Updated so it tests HW 3

--- Daniel version proj 2/mips.asm
+++ Daniel version proj 2/mips.asm
@@ -1,22 +1,51 @@
-addi $3,$0,8192
-addi $14,$0,4
-addi $5,$12,-4
-beq $5, $0, TEST
-bne $5, $0, TEST2
-sw $5, 1($3)
-lw $9, 1($3)
-sb $5, 4($3)
-lb $10, 4($3)
-addiu $11,$14,-4
-add $2,$4,$zero
-L1:add $1,$0,$8
-add $7,$7,$8
-TEST2:
-addi $3,$3,-2
-TEST: addi $2,$5,-1
-mult $14,$14
-multu $14,$14
-srl $14, $14,1
-slt $2,$14,$5
-sltu $2,$14,$5
-j L1
+addi $10, $0, 255
+addi $12, $0, 1
+addi $13, $0, 8192
+
+loop:
+sb $12, 0($13)
+addi $13, $13, 1
+addi $10, $10, -1
+addi $12, $12, 1
+bne $10, $0, loop
+
+addi $12, $12, -1
+sb $12, 0($13)
+
+addi $10, $0, 256
+addi $13, $0, 8192
+addi $14, $0, 0
+addi $15, $0, 8
+addi $16, $0, 0
+addi $17, $0, 0
+addi $18, $0, 0
+addi $19, $0, 0
+
+loop2:
+lb $14, 0($13)
+
+loop_and:
+andi $18, $14, 1
+
+beq $18, $0, skip
+addi $17, $17, 1
+
+skip:
+srl $14, $14, 1
+addi $15, $15, -1
+bne $15, $0, loop_and
+
+
+addi $15, $0, 8
+sub $16, $15, $17
+
+bne $16,$17, skip2
+
+addi $19,$19,1
+skip2:
+
+addi $16, $0, 0
+addi $17, $0, 0
+addi $13, $13, 1
+addi $10, $11, -1
+bne $10, $0, loop2
